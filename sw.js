dcb04b142b21662ab96e3bef4c2d9af8426997df
thidaka45 <thidaka45@gmail.com>
2026-07-07 10:02:53 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //    例: v4 → v5 → v6 ...
 //    これによりスマホ側のキャッシュが自動更新されます
 // ============================================================
-const CACHE_VERSION = "kakeibo-v12";
+const CACHE_VERSION = "kakeibo-v13";
 const ASSETS = ["./index.html", "./manifest.json", "./sw.js"];
 
 // インストール時: 新しいキャッシュを作成
